400d662a33719fe6144a5a44e6ccd09d0603a518
Micah Asowata <andrewgary963@gmail.com>
2023-04-25 18:02:39 +0100

    Variable Declaration 1: Using var

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,12 +1,3 @@
-// This does not need a comment for obvious reasons
-function reverseString(text) {
-  let reversedValue = "";
+var hello = "Hello";
 
-  text.split("").forEach((char) => {
-    reversedValue = char + reversedValue;
-  });
-
-  return reversedValue;
-}
-
-console.log(reverseString("Micah"));
+console.log(hello);
