3dcc61018bc2a36c2ec6dbfb8f846a4b9b39ae60
Micah Asowata <andrewgary963@gmail.com>
2023-04-25 17:34:08 +0100

    Multi and single line comments

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,2 +1,9 @@
-// TODO: Add some emojis 'cos they rock
-console.log("Hello, infinity and beyond!");
+/**
+ * greeting greets you. It is has simply as that.
+ */
+
+function greeting(name) {
+  return `Hello ${name}`;
+}
+
+console.log(greeting("Micah")); // Displays: Hello Micah
