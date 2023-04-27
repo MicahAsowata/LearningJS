697256651cf23f97a79f7f3e95f0297859b82890
Micah Asowata <andrewgary963@gmail.com>
2023-04-27 21:15:37 +0100

    Checking data types using typeof

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,3 +1,20 @@
-const fun = true;
+const people = ["Micah", "Aaron", "Burke", "Christopher"];
+const one = 1;
+const msg = "Hello 🌍";
+const b = true;
+const person = {
+  firstName: "Micah",
+  lastName: "Asowata",
+};
 
-console.log(`JavaScript is not fun is a ${!fun} statement`);
+const sayHello = (person) => {
+  console.log(person.firstName);
+};
+
+console.log("-- typeof --");
+console.log(typeof people);
+console.log(typeof one);
+console.log(typeof msg);
+console.log(typeof b);
+console.log(typeof person);
+console.log(typeof sayHello);
