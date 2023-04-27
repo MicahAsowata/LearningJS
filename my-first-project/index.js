dee1da4c730f4b10b87fe4f6ef52160b314dcca7
Micah Asowata <andrewgary963@gmail.com>
2023-04-27 21:48:04 +0100

    Strings to Floats

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,7 +1,6 @@
 const num = "150";
 const flo = "1.50";
 
-console.log("-- Strings to Int --");
-console.log(parseInt(num));
+console.log("-- Strings to Float --");
+console.log(parseInt(flo));
 console.log(parseInt("ABC"));
-console.log(parseInt("0xF"));
