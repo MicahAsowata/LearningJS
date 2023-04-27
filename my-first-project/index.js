ea1c68f62215b3162a04de019aea086f17872e7b
Micah Asowata <andrewgary963@gmail.com>
2023-04-27 21:28:11 +0100

    Basic Maths

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,20 +1,7 @@
-const people = ["Micah", "Aaron", "Burke", "Christopher"];
-const one = 1;
-const msg = "Hello 🌍";
-const b = true;
-const person = {
-  firstName: "Micah",
-  lastName: "Asowata",
-};
+const num = 100;
 
-const sayHello = (person) => {
-  console.log(person.firstName);
-};
-
-console.log("-- instanceof --");
-console.log(people instanceof Array);
-console.log(one instanceof Number);
-console.log(msg instanceof String);
-console.log(b instanceof Boolean);
-console.log(person instanceof Object);
-console.log(sayHello instanceof Function);
+console.log("-- Basic Math --");
+console.log(num + 25);
+console.log(num - 25);
+console.log(num * 25);
+console.log(num / 25);
