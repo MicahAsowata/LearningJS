d51cecf095c0bbc61f07cfecf3479894ea661996
Micah Asowata <andrewgary963@gmail.com>
2023-04-28 21:36:56 +0100

    If-Els-if-Else block in its tersest form

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,9 +1,5 @@
 const statusCode = 400;
 
-if (statusCode === 200) {
-  console.log("OK!");
-} else if (statusCode === 400) {
-  console.log("Error!");
-} else {
-  console.log("Unknown status");
-}
+if (statusCode === 200) console.log("OK!");
+else if (statusCode === 400) console.log("Error!");
+else console.log("Unknown status");
