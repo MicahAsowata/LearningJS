49d8e89e5b6222b22746515e36bed81c4d0b24ca
Micah Asowata <andrewgary963@gmail.com>
2023-04-28 21:33:35 +0100

    Changed the condition and it is all truthy again 😂😂

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,7 +1,9 @@
-const statusCode = 200;
+const statusCode = 400;
 
 if (statusCode === 200) {
   console.log("OK!");
+} else if (statusCode === 400) {
+  console.log("Error!");
 } else {
   console.log("Unknown status");
 }
