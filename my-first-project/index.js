072f11dc1452bc6ed0a87aa06119a3d65c26934e
Micah Asowata <andrewgary963@gmail.com>
2023-04-28 21:07:33 +0100

    Basic Time

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,17 +1,3 @@
-const criticalCode = () => {
-  throw "throw an exception";
-};
+const now = new Date();
 
-const logException = (theException) => {
-  console.log(theException);
-};
-
-console.log("-- Try Catch Finally --");
-try {
-  criticalCode();
-} catch (ex) {
-  console.log("handling the exception");
-  logException(ex);
-} finally {
-  console.log("This should work");
-}
+console.log(now);
