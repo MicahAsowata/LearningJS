108c04bea4fb0eef575a2a453b85c799de7909de
Micah Asowata <andrewgary963@gmail.com>
2023-04-28 20:53:52 +0100

    Try Catch Finally

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -6,10 +6,12 @@ const logException = (theException) => {
   console.log(theException);
 };
 
-console.log("-- Throwing in Try Catch --");
+console.log("-- Try Catch Finally --");
 try {
-  throw "Checking";
+  criticalCode();
 } catch (ex) {
   console.log("handling the exception");
   logException(ex);
+} finally {
+  console.log("This should work");
 }
