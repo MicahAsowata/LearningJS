d9f90ddc3d4e764863c825a8879afc8f15862b82
Micah Asowata <andrewgary963@gmail.com>
2023-04-30 23:48:37 +0100

    Accessing array values based on the index

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,6 +1,6 @@
-let usernames = Array(2);
+const usernames = ["Aaron", "Micah", "Ada"];
 
-console.log("-- Populating array after creation --");
-usernames[0] = "Aaron";
+console.log("-- Accessing values based on index --");
 console.log(usernames[0]);
-console.log(usernames[1]);
+console.log(usernames[usernames.length - 1]);
+console.log(usernames[usernames.length]);
