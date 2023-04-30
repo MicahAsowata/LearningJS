cbde8223f4213ad3541eec5ff3d6a1aa35ec0d99
Micah Asowata <andrewgary963@gmail.com>
2023-04-30 23:59:04 +0100

    Shifting array data

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,6 +1,6 @@
-let users = ["Jasmine", "Primeagen", "Micah"];
+let users = ["Aaron", "Jasmine", "Primeagen", "Micah"];
 
-console.log("-- Unshifting data into arrays --");
+console.log("-- Shifting array data --");
 console.log(users);
-console.log(users.unshift("Aaron"));
+console.log(users.shift());
 console.log(users);
