8203afeac7b23699cb9118f48b054e986dd2d208
Micah Asowata <andrewgary963@gmail.com>
2023-05-02 04:57:16 +0100

    Promises that are resolving

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,5 +1,13 @@
-function scope() {
-  return this;
+function promiseTimeout(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
 }
 
-console.log(scope() === globalThis);
+promiseTimeout(2000)
+  .then(() => {
+    console.log("It all works");
+  })
+  .catch(() => {
+    console.log("There is an error");
+  });
