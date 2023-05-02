e52a204c1314b0d7e603931691d279a30c1307e6
Micah Asowata <andrewgary963@gmail.com>
2023-05-02 05:20:44 +0100

    Basic async await usage

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -4,18 +4,10 @@ function promiseTimeout(ms) {
   });
 }
 
-promiseTimeout(2000)
-  .then(() => {
-    console.log("It all works");
-    return promiseTimeout(1000);
-  })
-  .then(() => {
-    console.log("It is working");
-    return Promise.reject(42);
-  })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch(() => {
-    console.log("There is an error");
-  });
+async function run() {
+  console.log("Start");
+  await promiseTimeout(2000);
+  console.log("stop");
+}
+
+run();
