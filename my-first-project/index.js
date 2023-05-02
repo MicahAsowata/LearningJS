36aea20578e925df5702d2743e632ae3082afe1c
Micah Asowata <andrewgary963@gmail.com>
2023-05-02 05:10:45 +0100

    Handling promises

--- my-first-project/index.js
+++ my-first-project/index.js
@@ -1,12 +1,20 @@
 function promiseTimeout(ms) {
   return new Promise((resolve, reject) => {
-    setTimeout(reject, ms);
+    setTimeout(resolve, ms);
   });
 }
 
 promiseTimeout(2000)
   .then(() => {
     console.log("It all works");
+    return promiseTimeout(1000);
+  })
+  .then(() => {
+    console.log("It is working");
+    return Promise.reject(42);
+  })
+  .then((result) => {
+    console.log(result);
   })
   .catch(() => {
     console.log("There is an error");
